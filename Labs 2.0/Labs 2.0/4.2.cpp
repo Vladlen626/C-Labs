3f0fab82f6ad7896c9d4f4e984cd834cc67a5404
Vladlen <vladlenfrog@gmail.com>
2021-10-11 13:41:59 +0300

    Закончил 4 лабу

--- Labs 2.0/Labs 2.0/4.2.cpp
+++ Labs 2.0/Labs 2.0/4.2.cpp
@@ -1,4 +1,5 @@
 #include <iostream>;
+
 using namespace std;
 
 class Date {
@@ -16,43 +17,141 @@ public:
 		month = _month;
 		year = _year;
 	}
+
 	~Date() {};
+
 
 	friend ostream& operator<<(ostream& out,Date& ob);
 	Date operator+(Date& ob);
 	Date operator-(Date& ob);
+	Date operator++(int _day);
+	Date operator--(int _day);
 	Date operator+(int day);
-	Date operator+(Date& ob);
-
+	Date operator-(int day);
+	
 };
+
+Date Date::operator--(int _day) {
+	Date temp;
+	temp.day = day-1;
+	temp.month = month;
+	temp.year = year;
+	
+		if (temp.month == 2 or temp.month == 4 or temp.month == 1 or temp.month == 6 or temp.month == 11 or temp.month == 8 or temp.month == 9) {
+			if (temp.day < 1) {
+				temp.day += 31;
+				temp.month--;
+			}
+		}
+		else if (temp.month == 3) {
+			if (temp.day < 1) {
+				temp.day += 28;
+				temp.month--;
+			}
+		}
+		else {
+			if (temp.day < 1) {
+				temp.day += 30;
+				temp.month;
+			}
+		}
+	
+	while (temp.month < 1) {
+		temp.year--;
+		temp.month + 12;
+	}
+	return temp;
+}
+
+Date Date::operator++(int _day) {
+	Date temp;
+	temp.day = day+1;
+	temp.month = month;
+	temp.year = year;
+		if (temp.month == 1 or temp.month == 3 or temp.month == 12 or temp.month == 5 or temp.month == 10 or temp.month == 7 or temp.month == 8) {
+			if (temp.day > 31) {
+				temp.day = 1;
+				temp.month++;
+			}
+		}
+		else if (temp.month == 2) {
+			if (temp.day > 28) {
+				temp.day = 1;
+				temp.month++;
+			}
+		}
+		else {
+			if (temp.day > 30) {
+				temp.day = 1;
+				temp.month++;
+			}
+		}
+	
+	while (temp.month > 12) {
+		temp.month - 12;
+		temp.year++;
+	}
+	return temp;
+}
 
 Date Date::operator+(int _day) {
 	Date temp;
 	temp.day = day + _day;
 	temp.month = month;
 	temp.year = year;
-	if (temp.month == 1 or temp.month == 3 or temp.month == 4 or temp.month == 5 or temp.month == 6 or temp.month == 7 or temp.month == 8) {
-		if (temp.day > 31) {
-			temp.day - 31;
-			temp.month++;
+		if (temp.month == 1 or temp.month == 3 or temp.month == 12 or temp.month == 5 or temp.month == 10 or temp.month == 7 or temp.month == 8) {
+			if (temp.day > 31) {
+				temp.day -= 31;
+				temp.month++;
+			}
 		}
-	}
-	else if (temp.month == 2) {
-		if (temp.day > 28) {
-			temp.day - 28;
-			temp.month++;
+		else if (temp.month == 2) {
+			if (temp.day > 28) {
+				temp.day -= 28;
+				temp.month++;
+			}
 		}
-	}
-	else {
-		if (temp.day > 30) {
-			temp.day = 30;
-			temp.month++;
+		else {
+			if (temp.day > 30) {
+				temp.day -= 30;
+				temp.month++;
+			}
 		}
-	}
-
 	while (temp.month > 12) {
-		temp.month - 12;
+		temp.month -= 12;
 		temp.year++;
+	}
+	return temp;
+}
+
+Date Date::operator-(int _day) {
+	Date temp;
+	temp.day = day - _day;
+	temp.month = month;
+	temp.year = year;
+
+		if (temp.month == 2 or temp.month == 4 or temp.month == 1 or temp.month == 6 or temp.month == 11 or temp.month == 8 or temp.month == 9) {
+			if (temp.day < 1) {
+				temp.day += 31;
+				temp.month--;
+			}
+		}
+		else if (temp.month == 3) {
+			if (temp.day < 1) {
+				temp.day += 28;
+				temp.month--;
+			}
+		}
+		else {
+			if (temp.day < 1) {
+				temp.day += 30;
+				temp.month--;
+			}
+		}
+	
+	while (temp.month < 1) {
+		temp.year--;
+		temp.month += 12;
 	}
 	return temp;
 }
@@ -61,28 +160,27 @@ Date Date::operator+(Date& ob) {
 	Date temp;
 
 	temp.month = month + ob.month;
-
 	temp.day = day + ob.day;
-	if (temp.month == 1 or temp.month == 3 or temp.month == 4 or temp.month == 5 or temp.month == 6 or temp.month == 7 or temp.month == 8) {
-		if (temp.day > 31) {
-			temp.day - 31;
-			temp.month++;
+		if (temp.month == 1 or temp.month == 3 or temp.month == 12 or temp.month == 5 or temp.month == 10 or temp.month == 7 or temp.month == 8) {
+			if (temp.day > 31) {
+				temp.day -= 31;
+				temp.month++;
+			}
 		}
-	}else if (temp.month == 2) {
-		if (temp.day > 28) {
-			temp.day - 28;
-			temp.month++;
+		else if (temp.month == 2) {
+			if (temp.day > 28) {
+				temp.day -= 28;
+				temp.month++;
+			}
 		}
-	}
-	else {
-		if (temp.day > 30) {
-			temp.day = 30;
-			temp.month++;
+		else {
+			if (temp.day > 30) {
+				temp.day -= 30;
+				temp.month++;
+			}
 		}
-	}
-	
 	while (temp.month > 12) {
-		temp.month - 12;
+		temp.month -= 12;
 		temp.year++;
 	}
 	temp.year += year + ob.year;
@@ -92,40 +190,58 @@ Date Date::operator+(Date& ob) {
 Date Date::operator-(Date& ob) {
 	Date temp;
 	temp.month = month - ob.month;
-
 	temp.day = day - ob.day;
-	if (temp.month == 2 or temp.month == 4 or temp.month == 5 or temp.month == 6 or temp.month == 7 or temp.month == 8 or temp.month == 9) {
-		if (temp.day < 1) {
-			31+temp.day;
-			temp.month--;
+	while (temp.day > 31 or temp.day < 1) {
+		if (temp.month == 2 or temp.month == 4 or temp.month == 1 or temp.month == 6 or temp.month == 11 or temp.month == 8 or temp.month == 9) {
+			if (temp.day < 1) {
+				temp.day += 31;
+				temp.month--;
+			}
+		}
+		else if (temp.month == 3) {
+			if (temp.day > 28) {
+				temp.day += 28;
+				temp.month--;
+			}
+		}
+		else {
+			if (temp.day < 1) {
+				temp.day += 30;
+				temp.month--;
+			}
 		}
 	}
-	else if (temp.month < 1) {
-		if (temp.day > 28) {
-			28 + temp.day;
-			temp.month--;
-		}
-	}
-	else {
-		if (temp.day < 1) {
-			30 + temp.day;
-			temp.month--;
-		}
-	}
-
 	while (temp.month < 1) {
 		temp.year--;
-		temp.month + 12;
+		temp.month += 12;
 	}
 	temp.year = year - ob.year;
 	return temp;
 }
 
 ostream& operator<<(ostream& out, Date& ob){
-	out << ob.day << "." << ob.month<<"."<< ob.year;
+
+	return out << ob.day << "." << ob.month<<"."<< ob.year;
 }
 
-int maint() {
-	
+int main() {
+	setlocale(LC_ALL, "Russian");
+	Date	start(1, 2, 2010), constructing(29, 0, 0);
+	Date end = start + constructing;
+
+
+	cout << "Дата начала строительства: " << start << endl;
+	cout << "Время строительства: " << constructing << endl;
+	cout << "Дата сдачи объекта(Дата+Дата): " << end << endl;
+	end = start - constructing;
+	cout << "Дата начала строительства объекта(Дата-Дата): " << end << endl;
+	end = start + 29;
+	cout << "Дата сдачи объекта(Дата+Int): " << end << endl;	
+	end = end - 29;
+	cout << "Дата начала строительства объекта(Дата-Int): " << end << endl;
+	end = start++;
+	cout << "Дата сдачи объекта(Дата++): " << end << endl;
+	end = end--;
+	cout << "Дата начала строительства объекта(Дата--): " << end << endl;
 	return 0;
 }
